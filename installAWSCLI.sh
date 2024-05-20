8b92c2cdfd3f5104489a78fecee43d7c2c71cab7
BoniaBenny <85350031+BoniaBenny@users.noreply.github.com>
2024-05-20 12:09:58 +0530

    Update installAWSCLI.sh

--- installAWSCLI.sh
+++ installAWSCLI.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# the purpose of this script is to install the AWS CLI
+
+# the purpose of this script is to create AWS VM
+
 
 AWS_ACCESS_KEY_ID=''
 AWS_SECRET_ACCESS_KEY=''
-ARCH=$(uname -m)
 
 showHelp () {
         cat << EOF
@@ -63,12 +64,90 @@ else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
-
-
 # install aws cli 
 apt update
 apt install -y curl unzip groff
 curl "$AWS_CLI_URL" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
+
+# list vms
+aws ec2 describe-instances
+
+# install jq
+apt-get install jq -y
+
+# create a key pair
+aws ec2 create-key-pair --key-name My_Pair --query 'KeyMaterial' --output text > My_KeyPair.pem
+
+echo "****"
+cat My_KeyPair.pem
+echo "****"
+
+# get vpcid
+VPC_ID=$(aws ec2 describe-vpcs | jq -e -r ".Vpcs[0].VpcId")
+
+# get first subnet associated with VPC_ID
+SUBNET_ID=$(aws ec2 describe-subnets --filter="Name=vpc-id,Values=${VPC_ID}" |  jq -e -r ".Subnets[0].SubnetId")
+
+# hardcode the AMI id for now
+AMI_ID=ami-0506d6d51f1916a96
+
+# Create a security group
+aws ec2 create-security-group \
+    --group-name my-sg \
+    --description "AWS ec2 CLI MY SG" \
+    --tag-specifications 'ResourceType=security-group,Tags=[{Key=Name,Value=my-sg}]' \
+    --vpc-id "${VPC_ID}"
+
+# get the security group id
+SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filter="Name=group-name,Values=my-sg" | jq -e -r ".SecurityGroups[0].GroupId")
+
+# create an ingress rule for ssh access
+aws ec2 authorize-security-group-ingress \
+    --group-id ${SECURITY_GROUP_ID} \
+    --protocol tcp \
+    --port 22 \
+    --cidr 0.0.0.0/0
+
+# launch the instance
+aws ec2 run-instances \
+  --image-id ${AMI_ID} \
+  --count 1 \
+  --instance-type t3.micro \
+  --key-name My_Pair \
+  --security-group-ids ${SECURITY_GROUP_ID} \
+  --subnet-id ${SUBNET_ID} \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=tektontest1}]'
+
+  # test the copying of a script to new instance
+  echo "#!/bin/bash" >> testscript.sh
+  echo "echo \$(hostname)" >> testscript.sh
+  chmod 755 testscript.sh
+
+  # install scp
+  apt-get install openssh-client -y
+
+  # get the instanceID
+  INSTANCE_ID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=tektontest1 Name=instance-state-name,Values=running | jq -e -r ".Reservations[].Instances[].InstanceId")
+
+  # get the instance PublicDNS
+  PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --query 'Reservations[].Instances[].PublicDnsName' | jq -e -r ".[]")
+
+  #sleep for 20 secs, looks like it takes a while for the security group rule to activate
+  echo "sleeping for 20 secs"
+  sleep 20
+
+  # reduce permissions on .pem file
+  chmod 400 My_KeyPair.pem
+
+
+  # scp file over to new instance and execute it 
+  scp  -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i My_KeyPair.pem -r ./testscript.sh admin@${PUBLIC_DNS}:~/
+
+  ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i My_KeyPair.pem admin@${PUBLIC_DNS} './testscript.sh'
+
+
+
+
 
