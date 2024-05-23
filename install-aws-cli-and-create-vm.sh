f1434913fa4ab6f9e02cf13857fb29767b76067a
Bonia Benny <Bonia.Benny@ibm.com>
2024-05-23 13:21:09 +0530

    Updating s3 vm actions

--- install-aws-cli-and-create-vm.sh
+++ install-aws-cli-and-create-vm.sh
@@ -81,7 +81,7 @@ echo "****"
 apt-get install jq -y
 
 # create a key pair
-aws ec2 create-key-pair --key-name My_Pair11 --query 'KeyMaterial' --output text > My_KeyPair1.pem
+aws ec2 create-key-pair --key-name My_Pair12 --query 'KeyMaterial' --output text > My_KeyPair1.pem
 
 echo "****"
 cat My_KeyPair1.pem
@@ -104,21 +104,21 @@ AMI_ID=ami-0506d6d51f1916a96
 #     --vpc-id "${VPC_ID}"
 
 # get the security group id
-SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filter="Name=group-name,Values=my-sg2" | jq -e -r ".SecurityGroups[0].GroupId")
+SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filter="Name=group-name,Values=my-sg" | jq -e -r ".SecurityGroups[0].GroupId")
 
 # create an ingress rule for ssh access
-# aws ec2 authorize-security-group-ingress \
-#     --group-id ${SECURITY_GROUP_ID} \
-#     --protocol tcp \
-#     --port 22 \
-#     --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress \
+    --group-id ${SECURITY_GROUP_ID} \
+    --protocol tcp \
+    --port 22 \
+    --cidr 0.0.0.0/0
 
 # launch the instance
 aws ec2 run-instances \
   --image-id ${AMI_ID} \
   --count 1 \
   --instance-type t3.micro \
-  --key-name My_Pair11 \
+  --key-name My_Pair12 \
   --security-group-ids ${SECURITY_GROUP_ID} \
   --subnet-id ${SUBNET_ID} \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=tektontest1}]'
